6757d6470a0f2acb033656fe7e51609d2ab01748
Everton da S. Serafim <thi.ger21@gmail.com>
2023-05-04 19:02:00 -0300

    Criando o Componente Heading e sua estilização

---
+++ src/stories/Heading.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Heading, { HeadingProps } from "../components/Typography/Heading";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Heading> = {
+  title: "Example/Heading",
+  component: Heading,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
+  //**************************************************************************//
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Heading1: Story = {
+  args: {
+    level: 1,
+    children: 'Heading 1'
+  },
+};
+export const Heading2: Story = {
+  args: {
+    level: 1,
+    children: 'Heading 2'
+  },
+};
+export const Heading3: Story = {
+  args: {
+    level: 1,
+    children: 'Heading 3'
+  },
+};
---
+++ src/stories/ValueDescriptor.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ValueDescriptior, { ValueDescriptorProps } from "../components/ValueDescriptor/ValueDescriptor";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof ValueDescriptior> = {
+  title: "Example/ValueDescriptior",
+  component: ValueDescriptior,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
+  //**************************************************************************//
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
+
+export default meta;
+type Story = StoryObj<typeof ValueDescriptior>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Default: Story = {
+  args: {
+    description: 'Ganhos na semana',
+    value: 560322.02
+  },
+};
+export const Primary: Story = {
+  args: {
+    description: 'Ganhos na semana',
+    isCurrency: false,
+    value: 560322.02
+  },
+};
+export const DefaultCurrency: Story = {
+  args: {
+    description: 'Ganhos na semana',
+    isCurrency: true,
+    value: 560322.02
+  },
+};
+export const PrimaryCurrency: Story = {
+  args: {
+    description: 'Ganhos na semana',
+    isCurrency: true,
+    value: 560322.02
+  },
+};
