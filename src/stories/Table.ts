7a92972fa726628b019661117f19ef6fd944f5cb
Everton da S. Serafim <thi.ger21@gmail.com>
2023-05-08 18:16:44 -0300

    Inicialização da criação da Tabela.

---
+++ src/stories/Table.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Table from "../components/Table/Table";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Table> = {
+  title: "Example/Table",
+  component: Table,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
+  //**************************************************************************//
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
+
+export default meta;
+type Story = StoryObj<typeof Table>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Default: Story = {
+  args: {
+    
+  },
+};
