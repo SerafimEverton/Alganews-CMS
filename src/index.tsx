e96acbf117d38a3734af298cdff6b2533e128532
Everton da S. Serafim <thi.ger21@gmail.com>
2023-07-17 18:50:24 -0300

    Finalizando os

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound404 from './views/NotFound404.view';
+import Contact from './views/Contact.view';
+import Home from './views/Home.view';
+import UserView from './views/User.view';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <NavBar />
+    <BrowserRouter>
+    <Switch>
+      <Route path={'/home'} exact Component={Home} />
+      
+      <Route path={'/usuario:userId'} exact Component={UserView} />
+
+      <Route path={'/calc/:a/:b'} Component={CalcView} />
+
+      <Route path={'/contact'} exact Component={Contact} />
+      
+      <Route Component={NotFound404} />
+       
+    </Switch>   
+    </BrowserRouter>
+    </div> 
   </React.StrictMode>
 );
 
---
+++ src/views/Contact.view.tsx
@@ -0,0 +1,11 @@
+import usePageTitle from "../Hooks/usePageTitle"
+
+export default function Contact() {
+
+  usePageTitle('Contact')
+  
+    return <div>
+    <h1>Contact</h1>
+    </div>
+  }
+  
