11c9bc8c4cb0bd0e257958ed4e57bf77970830db
Everton da S. Serafim <thi.ger21@gmail.com>
2023-08-04 18:27:23 -0300

    Criando o Error Boundary

--- src/app/views/EditorProfile.view.tsx
+++ src/app/views/EditorProfile.view.tsx
@@ -1,9 +1,12 @@
 import DefaultLayout from "../../Layout/Default/Default.layout";
+import ErrorBoundary from "../components/ErrorBoundary";
 import EditorProfile from "../components/Features/EditorProfile.feature";
 
 function EditorProfileView(){
     return<DefaultLayout>    
-    <EditorProfile />
+    <ErrorBoundary>
+    <EditorProfile hidePersonalData />
+    </ErrorBoundary>
     </DefaultLayout>}
 
     export default EditorProfileView
